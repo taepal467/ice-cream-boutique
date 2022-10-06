6757c8d2c662ed07f5c9079191f77b39ad710e59
Chantae Palacio <chantae.palacio@gmail.com>
2022-10-06 15:35:16 -0700

    Deleted the html plug in once again.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -8,7 +7,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
